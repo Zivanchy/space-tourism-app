9f2995c0bdc43e072504ff83340ec5fff90311f7
Zivan Madjarev <zivanmadjarev@gmail.com>
2021-12-04 17:18:30 +0100

    added home page and implemented styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,23 @@ import backgroundImage from '../../assets/home/background-home-desktop.jpg';
 
 const Home = () => {
   return (
-    <div className="home" style={{ backgroundImage: backgroundImage }}></div>
+    <div
+      className="home"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="home__container">
+        <h2 className="home__container-subtitle">So, you want to travel to</h2>
+        <h1 className="home__container-title">Space</h1>
+        <p className="home__container-text">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </p>
+      </div>
+
+      <button className="home__button">Explore</button>
+    </div>
   );
 };
 
